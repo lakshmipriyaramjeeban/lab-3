0d8e3df0a55ca382130ca34ae82cb35853f000b2
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-09-06 17:17:10 +0530

    Create problem4.cpp

---
+++ problem4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+	int a;
+	cout<<"enter the number:";
+	cin>>a;
+	if((a%5==0)&&(a%11==0))
+	{
+		cout<<"a is divisible by 5 and 11:";
+	}
+	else
+	{
+		cout<<"a is not divisible by 5 and 11:";
+	}
+	return 0;
+}
