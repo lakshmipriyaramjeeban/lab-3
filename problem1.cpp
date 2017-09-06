167db2978e255464344221efd69a5bcb90a476df
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-09-06 16:28:42 +0530

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int a,b;
+	cout<<"enter two num:";
+	cin>>a>>b;
+	if(a>b)
+	{
+		cout<<"a is the larger no:";
+	}
+	else
+	{
+		cout<<"b is the larger no:";
+	}
+	return 0;
+}
