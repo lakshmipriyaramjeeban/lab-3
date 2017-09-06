09d96a88e1f0f153384c6476a14ace22e4e7b152
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-09-07 00:27:34 +0530

    Create problem7.cpp

---
+++ problem7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	char c;
+	cout<<"enter a character:";
+	cin>>c;
+	if((c>='a' && c<='z') || (c>='A' && c<='Z'))
+	{
+		cout<<"c is an alphabet:";
+	}
+	else
+	{
+		cout<<"c is not an alphabet:";
+	}
+	return 0;
+}
