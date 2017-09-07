84bd5406af4def17c6337a4093061fa965b2e41e
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-09-07 22:05:12 +0530

    Create problem28.cpp

---
+++ problem28.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n,i,j;
+	cout<<"enter n";
+	cin>>n;
+	i=0,j=0;
+	while(i<=n)
+	{
+		if(i%2==0)
+		j=j=i;
+		i+=1;
+		
+	}
+	cout<<j;
+	return 0;
+}
