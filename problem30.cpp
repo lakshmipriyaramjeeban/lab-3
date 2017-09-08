c0c345c2452b2ceaa9b63073ceeb6686f62ba865
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-09-08 20:12:53 +0530

    Create problem30.cpp

---
+++ problem30.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n,i,j;
+	cout<<"enter n=";
+	cin>>n;
+	i=1,j=0;
+	while(i<=10)
+	{
+		j=n*i;
+		cout<<n<<"x"<<i<<"="<<j<<"\n";
+		i+=1;
+	}
+	return 0;
+}
