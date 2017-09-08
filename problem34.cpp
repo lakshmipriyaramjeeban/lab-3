a8f239e94fef78582b9a73da6965b1897bcb08a3
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-09-08 23:02:23 +0530

    Create problem34.cpp

---
+++ problem34.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() 
+{
+	int a,i,f,l;
+	cout<<"enter number";
+	cin>>a;
+	f=a;
+	i=0;
+	while(f>10)
+	{
+		f=f/10;
+		i++;
+	}
+	l=a%10;
+	a=a-l+f;
+	a=(a-f*(pow(10,i)))+l*(pow(10,i));
+	cout<<a;
+	return 0;
+}
