edbe7d29df914f249fbdea341f0de6336ac54d13
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-09-12 22:51:52 +0530

    Create problem53.cpp

---
+++ problem53.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a,i,s=0;
+cout << "Enter a positive  integer : ";
+cin >> a;
+for( i=1 ; i<a ; i++)
+   {
+    if(a%i==0)
+       s += i;
+   }
+if(s==a)
+  cout << "PERFECT NUMBER";
+  else
+   cout << "NOT A PERFECT NUMBER";
+return 0;
+}
