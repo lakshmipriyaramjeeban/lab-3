20cdad2a855476324b48b8315195c95147dc6385
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-09-14 15:23:47 +0530

    Create problem68.cpp

---
+++ problem68.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+ 
+
+int main()
+{
+    long int dec,rem,quo;
+    int i=1,j,temp;
+    char hexa[100];
+    cout<<"PROGRAM TO CONVERT DECIMAL TO HEXADECIMAL";
+    cout<<"\n Enter any decimal number: ";
+    cin>>dec;
+    quo = dec;
+    while(quo!=0)
+    {
+      temp = quo % 16;
+      /*To convert integer into character*/
+      if( temp < 10)
+           temp =temp + 48;
+      else
+         temp = temp + 55;
+ 
+      hexa[i++]= temp;
+      quo = quo / 16;
+    }
+    cout<<"nEquivalent hexadecimal number of "<<dec<<" is : ";
+    for(j = i -1 ;j> 0;j--)
+    cout<<hexa[j];
+    cout<<endl;
+    return 0;
+}
